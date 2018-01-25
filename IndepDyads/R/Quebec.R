83005c60cf286ebb2d2c5a29911100843c863a63
Tim Riffe <tim.riffe@gmail.com>
2018-01-25 13:41:18 +0100

    sorting things into new repo for separate project

---
+++ IndepDyads/R/Quebec.R
@@ -0,0 +1,144 @@
+# Author: tim
+###############################################################################
+library(foreign)
+library(lubridate)
+setwd("/home/tim/git/IndepDyads/IndepDyads")
+
+# this is the MPIDR version from 2012, currently checking to see if it has been updated.
+path      <- "/home/tim/Data/Quebec/FrenchCanadian.individuals.2012-01-27/RPQA.MarcKlemp.individus.2012-01-27.sav"
+# TR: line to read file
+Q         <- read.spss(path)
+# TR: line to read file on MG's system
+# Hm <-   read.spss("U:/quebec/Quebec/Quebec/FrenchCanadian.individuals.2012-01-27/RPQA.MarcKlemp.individus.2012-01-27.sav")
+Q         <- as.data.frame(Q)
+
+#Q$Lf2 <- Q$dateDecesAnnee - Q$dateNaissAnnee 
+#image(log(acast(Q, dateDecesAnnee~Lf2,length)))
+# ---------------------------------------#
+# begin data prep                        #
+# ---------------------------------------#
+
+
+
+# missing month random assignment? or should I use month dist of known? Can always swap out
+ind <- (Q$dateNaissMois == 0 | is.na(Q$dateNaissMois)) & !is.na(Q$dateNaissAnnee)
+sum(ind)
+Q$dateNaissMois[ind] <- sample(1:12,size=sum(ind),replace=TRUE)
+
+days        <- c(31,28,31,30,31,30,31,31,30,31,30,31)
+names(days) <- 1:12
+# missing day is 15th always
+ind <- (Q$dateNaissJour == 0 | is.na(Q$dateNaissJour)) & !is.na(Q$dateNaissAnnee)
+Q$dateNaissJour[ind] <- floor(days[as.character(Q$dateNaissMois[ind])] / 2)
+
+
+# missing month random assignment? or should I use month dist of known? Can always swap out
+ind <- (Q$dateDecesMois == 0 | is.na(Q$dateDecesMois)) & !is.na(Q$dateDecesAnnee)
+Q$dateDecesMois[ind] <- sample(1:12,size=sum(ind),replace=TRUE)
+
+# missing day is 15th always
+ind <- (Q$dateDecesJour == 0 | is.na(Q$dateDecesJour)) & !is.na(Q$dateDecesAnnee)
+Q$dateDecesJour[ind] <- floor(days[as.character(Q$dateDecesMois[ind])] / 2)
+
+# there appear to be some cases of sept 31st, and similar. Catch and make day valid
+# harmless to deduct a day from feb 29, for example.
+ind <- Q$dateNaissJour > days[as.character(Q$dateNaissMois)]
+ind[is.na(ind)] <- FALSE
+Q$dateNaissJour[ind] <- Q$dateNaissJour[ind] - 3
+
+ind <- Q$dateDecesJour > days[as.character(Q$dateDecesMois)]
+ind[is.na(ind)] <- FALSE
+Q$dateDecesJour[ind] <- Q$dateDecesJour[ind] - 3
+
+# compose Birthday
+Q$BD   	  <- as.Date(with(Q, paste(dateNaissAnnee, 
+						dateNaissMois, 
+						dateNaissJour, 
+						sep = "-")))
+
+#compose Deathday, need to hangle NAs
+DD 		  <- with(Q, paste(dateDecesAnnee, 
+				dateDecesMois, 
+				dateDecesJour, 
+				sep = "-"))
+
+# missing days/months appear to have been imputed, good!
+#any(is.na(Q$dateDecesMois) & !is.na(Q$dateDecesAnnee))
+#any(is.na(Q$dateDecesJour) & !is.na(Q$dateDecesMois))
+DD[is.na(Q$dateDecesAnnee)] 	<- NA
+Q$DD 							<- as.Date(DD)
+
+#any(is.na(Q$DD) & !is.na(Q$dateDecesAnnee))
+#any(is.na(Q$BD) & !is.na(Q$dateNaissAnnee))
+# calculate decimal lifespan
+Q$L       <- decimal_date(Q$DD) - decimal_date(Q$BD)
+Q$L[Q$L < 0] <- 0
+
+
+# LP plane
+# loop over year and L
+range(Q$L,na.rm=TRUE) # 0-108
+
+# dateNaissAnnee
+# dateDecesAnnee
+#range(Q$dateNaissAnnee, na.rm=TRUE)
+#range(Q$dateDecesAnnee, na.rm=TRUE)
+nrow(Q)
+Q <- Q[!is.na(Q$L), ]
+
+years <- 1572:1870
+Q$Lf <- floor(Q$L)
+LP <- matrix(NA, ncol = length(years), nrow = 109, dimnames = list(0:108,years))
+for (l in 0:108){
+	for (p in 1:length(years)){
+		LP[l+1,p] <- sum(Q$Lf == l & 
+						Q$dateNaissAnnee <= years[p] & 
+						Q$dateDecesAnnee >= years[p], na.rm = TRUE)
+	}
+}
+
+# Re Joel's epidemic comment
+epidemicl <- c(1732,1755,1759,1761,1772,1775,1702,1687,1714)
+epidemicr <- c(1733,1757,1759,1761,1772,1776,1703,1687,1715)
+
+
+colramp <- colorRampPalette(RColorBrewer::brewer.pal(9,"Blues"),space="Lab")
+brks <- seq(0,9,by=.5)
+png("Figures/QuebecLP.png",width=700,height=400)
+image(years,0:108,log(t(LP)),asp=1,breaks=brks,col=colramp(length(brks)-1),
+		xlab = "Period", ylab = "Length of life", ylim=c(0,100), las = 1)
+#contour(years,0:108,log(t(LP)),add=TRUE,levels=brks2,lwd=.5,col = gray(.2))
+rect(epidemicl,0,epidemicr,-10,border="red",xpd=TRUE)
+dev.off()
+
+library(reshape2)
+
+
+# CT
+library(reshape2)
+LC <- acast(Q,Lf~dateNaissAnnee)
+LC <- LC[-nrow(LC),]
+LC <- LC[,-ncol(LC)]
+
+LC <- LC[nrow(LC):1, ]
+TC <- apply(LC,2,cumsum)
+TC <- TC[nrow(TC):1, ]
+
+colramp <- colorRampPalette(RColorBrewer::brewer.pal(9,"Blues"),space="Lab")
+brks <- seq(0,9,by=.5)
+png("Figures/QuebecTC.png",width=700,height=400)
+image(as.integer(colnames(TC)),as.integer(rownames(TC)),log(t(TC)),asp=1,breaks=brks,col=colramp(length(brks)-1),
+		xlab = "Birth cohort", ylab = "Thanatological age", ylim=c(0,100),las=1)
+dev.off()
+
+# AD
+LD <- acast(Q,Lf~dateDecesAnnee)
+LD <- LD[-nrow(LD),]
+LD <- LD[,-ncol(LD)]
+LD <- LD[nrow(LD):1, ]
+AD <- apply(LD,2,cumsum)
+AD <- AD[nrow(AD):1, ]
+png("Figures/QuebecAD.png",width=700,height=400)
+image(as.integer(colnames(AD)),as.integer(rownames(AD)),log(t(AD)),asp=1,breaks=brks,col=colramp(length(brks)-1),
+		xlab = "Year of death", ylab = "Age", ylim=c(0,100),las=1)
+dev.off()
---
+++ IndepDyads/R/TetraOcta.R
@@ -0,0 +1,82 @@
+
+# construct a tetrahedral octahedral honecomb w cubic dual.
+
+
+library(rgl)
+#source("R/Functions.R")
+xyz2ternxyz <- function(xyz){
+	ternxyz   <- xyz
+	
+	ternxyz$x <- xyz$x - (xyz$y * .5)
+	ternxyz$y <- xyz$y * sqrt(3) / 2 + xyz$z * (sqrt(3) / 2 - 1 / sqrt(3))
+	ternxyz$z <- xyz$z * sqrt(6) / 3
+	ternxyz
+}
+
+# pp <- dget("Data/TALrglview2.R")
+
+startYr <- 1800
+endYr   <- 2100
+
+rgl.close()
+omega <- 125 # for the sake of dimensioning
+# =====================================
+# set up space, a box, a prelim aspect ratio (final adjustment later)
+plot3d(xlim = c(startYr, endYr), ylim = c(0, omega), zlim = c(0, omega), 
+		box = FALSE, 
+		aspect = c(3, 1, 1),
+		axes = FALSE, 
+		type ='n',
+		xlab = "",
+		ylab = "",
+		zlab = "")
+bg3d("white")
+# now 3 calendar lines
+cal1         <- xyz2ternxyz(data.frame(x = c(startYr, endYr), y = c(0, 0), z = c(0, 0)))
+leftagebound <- xyz2ternxyz(data.frame(x = c(startYr, startYr), y = c(0, omega), z = c(0, 0)))
+thanoaxis    <- xyz2ternxyz(data.frame(x = c(startYr, startYr), y = c(0, 0), z = c(0, omega)))
+#cal2 <- xyz2ternxyz(data.frame(x = c(startYr, endYr), y = c(omega, omega), z = c(0, 0)))
+cal3 <- xyz2ternxyz(data.frame(x = c(startYr, endYr-100), y = c(0,0), z = c(omega, omega)))
+
+rgl.linestrips(cal1$x, cal1$y, cal1$z, color = AssignColour("P"), lwd = 2)
+rgl.linestrips(leftagebound$x, leftagebound$y, leftagebound$z, color = AssignColour("A"), lwd = 2)
+#rgl.linestrips(cal2$x, cal2$y, cal2$z, color = AssignColour("P"), lwd = 2)
+rgl.linestrips(cal3$x, cal3$y, cal3$z, color = AssignColour("P"), lwd = 2)
+rgl.linestrips(thanoaxis$x, thanoaxis$y, thanoaxis$z, color = AssignColour("T"), lwd = 2)
+
+periods <- seq(startYr+25, endYr + omega, by = 25)
+for (i in 1:length(periods)){
+	cohi <- xyz2ternxyz(data.frame(x = c(periods[i], periods[i]), y = c(ifelse(periods[i]<endYr,0,periods[i]-endYr), omega), z = c(0, 0)))
+	rgl.linestrips(cohi$x, cohi$y, cohi$z, color = AssignColour("P"))
+}
+
+
+ages <- seq(25, omega, by = 25)
+for (i in 1:length(ages)){
+	cohi <- xyz2ternxyz(data.frame(x = c(startYr, endYr + ages[i]), y = ages[i], z = c(0, 0)))
+	rgl.linestrips(cohi$x, cohi$y, cohi$z, color = AssignColour("A"))
+}
+
+ns   <- seq(25,200,by=25)
+# an example rgl TAL (2000 birth cohort)
+for (n in ns){
+	xyztern <- xyz2ternxyz(data.frame(
+					x = c(2000, 2000+omega, 2000) - n,
+					y = c(0, omega, 0),
+					z = c(0, 0, omega)))
+	rgl.linestrips(xyztern$x[c(1,2)],xyztern$y[c(1,2)],xyztern$z[c(1,2)],color = gray(.5))
+	rgl.linestrips(xyztern$x[c(2,3)],xyztern$y[c(2,3)],xyztern$z[c(2,3)],color = gray(.5))
+	rgl.linestrips(xyztern$x[c(1,3)],xyztern$y[c(1,3)],xyztern$z[c(1,3)],color = gray(.5))
+}
+
+
+
+
+
+
+
+
+
+
+
+

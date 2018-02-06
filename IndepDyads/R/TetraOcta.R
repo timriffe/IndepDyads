e6e5f807aa7a6be6db47edcf060746458159208c
Tim Riffe <tim.riffe@gmail.com>
2018-02-06 17:00:52 +0100

    made rgl tetrahedrian with bimedians marked. Still working on adding medians

--- IndepDyads/R/TetraOcta.R
+++ IndepDyads/R/TetraOcta.R
@@ -3,7 +3,9 @@
 
 
 library(rgl)
+
 #source("R/Functions.R")
+
 xyz2ternxyz <- function(xyz){
 	ternxyz   <- xyz
 	
@@ -15,10 +17,72 @@ xyz2ternxyz <- function(xyz){
 
 # tetrahedron function
 # this will need apctdl logic
-tetrahedron_a <- function(){
-	
+tetverts <- function(){
+	verts <- data.frame(x=c(0,1,.5,.5),y=c(0,0,sqrt(3)/2,sqrt(3)/6),z=c(0,0,0,sqrt(3)/2))
+	rownames(verts) <- 1:4
+	verts
 }
 
+
+tetrahedron_a <- function(){
+	
+	# vertex coords
+	verts <- tetverts()
+	vx    <- verts$vx
+	vy    <- verts$vy
+	vz    <- verts$vz
+	
+	# vertex pairings
+	vi1 <- c(1,1,1,2,2,3)
+	vi2 <- c(2,3,4,3,4,4)
+	
+	edges <- data.frame(
+			edge = c("P","C","D","A","T","L"),
+			x1 = vx[vi1],
+			x2 = vx[vi2],
+			y1 = vy[vi1],
+			y2 = vy[vi2],
+			z1 = vz[vi1],
+			z2 = vz[vi2])
+	edges$xm <- (vx[vi1] + vx[vi2]) / 2
+	edges$ym <- (vy[vi1] + vy[vi2]) / 2
+	edges$zm <- (vz[vi1] + vz[vi2]) / 2
+	rownames(edges) <- edges$edge
+	edges
+}
+medians <- function(){
+	verts <- tetverts()
+	edges <- tetrahedron_a()
+	medians <- data.frame(x1 = verts$x,y1verts$y,z1=verts$z,
+			
+
+	x2 = c(verts$x[4]/3 + edges["A","xm"]*2/3,
+	       verts$x[3]/3 + edges["D","xm"]*2/3,
+           verts$x[4]/3 + edges["P","xm"]*2/3,
+           verts$x[1]/3 + edges["A","xm"]*2/3),
+	y2 = c(verts$y[4]/3 + edges["A","ym"]*2/3,
+			verts$y[3]/3 + edges["D","ym"]*2/3,
+			verts$y[4]/3 + edges["P","ym"]*2/3,
+			verts$y[1]/3 + edges["A","ym"]*2/3),
+	z2 = c(verts$z[4]/3 + edges["A","zm"]*2/3,
+			verts$z[3]/3 + edges["D","zm"]*2/3,
+			verts$z[4]/3 + edges["P","zm"]*2/3,
+			verts$z[1]/3 + edges["A","zm"]*2/3))
+	
+	rownames(medians) <- c("TAL","CDL","TPD","APC")
+	medians
+}
+
+AssignColour <- function (x) {
+	if (x == "A") result <- "#D23737"
+	if (x == "P") result <- "#3191C9"
+	if (x == "C") result <- "#D2BC2D"
+	if (x == "T") result <- "#4EC93B"
+	if (x == "D") result <- "#881F93"
+	if (x == "L") result <- "#C5752B"
+	return(result)
+}
+tet <- tetrahedron_a()
 
 
 # let's start with unit space: c(-2,2) for everything?
@@ -30,97 +94,46 @@ zlim <- c(-2,2)
 
 plot3d(xlim = xlim, ylim = ylim, zlim = zlim, 
 		box = FALSE, 
-		aspect = c(3, 1, 1),
+		aspect = c(1, 1, 1),
 		axes = FALSE, 
 		type ='n',
 		xlab = "",
 		ylab = "",
 		zlab = "")
 bg3d("white")
-
-
-
-
-
-
-# pp <- dget("Data/TALrglview2.R")
-
-startYr <- 1800
-endYr   <- 2100
-
-rgl.close()
-omega <- 125 # for the sake of dimensioning
-# =====================================
-# set up space, a box, a prelim aspect ratio (final adjustment later)
-plot3d(xlim = c(startYr, endYr), ylim = c(0, omega), zlim = c(0, omega), 
-		box = FALSE, 
-		aspect = c(3, 1, 1),
-		axes = FALSE, 
-		type ='n',
-		xlab = "",
-		ylab = "",
-		zlab = "")
-bg3d("white")
-# now 3 calendar lines
-cal1         <- xyz2ternxyz(data.frame(x = c(startYr, endYr), y = c(0, 0), z = c(0, 0)))
-leftagebound <- xyz2ternxyz(data.frame(x = c(startYr, startYr), y = c(0, omega), z = c(0, 0)))
-thanoaxis    <- xyz2ternxyz(data.frame(x = c(startYr, startYr), y = c(0, 0), z = c(0, omega)))
-#cal2 <- xyz2ternxyz(data.frame(x = c(startYr, endYr), y = c(omega, omega), z = c(0, 0)))
-cal3 <- xyz2ternxyz(data.frame(x = c(startYr, endYr-100), y = c(0,0), z = c(omega, omega)))
-
-rgl.linestrips(cal1$x, cal1$y, cal1$z, color = AssignColour("P"), lwd = 2)
-rgl.linestrips(leftagebound$x, leftagebound$y, leftagebound$z, color = AssignColour("A"), lwd = 2)
-#rgl.linestrips(cal2$x, cal2$y, cal2$z, color = AssignColour("P"), lwd = 2)
-rgl.linestrips(cal3$x, cal3$y, cal3$z, color = AssignColour("P"), lwd = 2)
-rgl.linestrips(thanoaxis$x, thanoaxis$y, thanoaxis$z, color = AssignColour("T"), lwd = 2)
-
-periods <- seq(startYr+25, endYr + omega, by = 25)
-for (i in 1:length(periods)){
-	cohi <- xyz2ternxyz(data.frame(x = c(periods[i], periods[i]), y = c(ifelse(periods[i]<endYr,0,periods[i]-endYr), omega), z = c(0, 0)))
-	rgl.linestrips(cohi$x, cohi$y, cohi$z, color = AssignColour("P"))
+for (i in 1:nrow(tet)){
+  rgl.linestrips(
+		x = c(tet$x1[i], tet$x2[i]), 
+		y = c(tet$y1[i], tet$y2[i]), 
+		z = c(tet$z1[i], tet$z2[i]), 
+		color = AssignColour(tet$edge[i]), lwd = 4)
+  text3d(x = tet$xm[i], y = tet$ym[i], z = tet$zm[i], tet$edge[i], col = AssignColour(tet$edge[i]), cex = 2)
+}
+verts <- tetverts()
+# label vertices for now:
+for (i in 1:4){
+	text3d(x = verts$x,verts$y,verts$z,1:4, cex = 2,col="black")
 }
 
-
-ages <- seq(25, omega, by = 25)
-for (i in 1:length(ages)){
-	cohi <- xyz2ternxyz(data.frame(x = c(startYr, endYr + ages[i]), y = ages[i], z = c(0, 0)))
-	rgl.linestrips(cohi$x, cohi$y, cohi$z, color = AssignColour("A"))
+# the three independant view axes (bimedian)
+indeps <- matrix(c("A","T","L","D","C","P"),ncol=2)
+for (i in 1:3){
+	rgl.linestrips(
+			x = c(tet[indeps[i,1],"xm"], tet[indeps[i,2],"xm"]), 
+			y = c(tet[indeps[i,1],"ym"], tet[indeps[i,2],"ym"]), 
+			z = c(tet[indeps[i,1],"zm"], tet[indeps[i,2],"zm"]), 
+			color = "#FF00FF50", lwd = 2)
 }
 
-ns   <- seq(25,200,by=25)
-# an example rgl TAL (2000 birth cohort)
-for (n in ns){
-	xyztern <- xyz2ternxyz(data.frame(
-					x = c(2000, 2000+omega, 2000) - n,
-					y = c(0, omega, 0),
-					z = c(0, 0, omega)))
-	rgl.linestrips(xyztern$x[c(1,2)],xyztern$y[c(1,2)],xyztern$z[c(1,2)],color = gray(.5))
-	rgl.linestrips(xyztern$x[c(2,3)],xyztern$y[c(2,3)],xyztern$z[c(2,3)],color = gray(.5))
-	rgl.linestrips(xyztern$x[c(1,3)],xyztern$y[c(1,3)],xyztern$z[c(1,3)],color = gray(.5))
+# the four triad normal view axes (medians)
+triads <- c("APC","TPD","CDL","TAL")
+meds <- medians()
+for (i in 1:4){
+
+	
+	rgl.linestrips(
+			x = c(meds[triads[i],"x1"], meds[triads[i],"x2"]), 
+			y = c(meds[triads[i],"y1"], meds[triads[i],"y2"]), 
+			z = c(meds[triads[i],"z1"], meds[triads[i],"z2"]), 
+			color = "#00FFFF50", lwd = 2)
 }
-
-
-
-# equal aspect ratio
-x <- diff(par3d("bbox")[1:2])
-y <- diff(par3d("bbox")[3:4])
-z <- diff(par3d("bbox")[5:6])
-decorate3d(xlim = c(startYr, endYr), ylim = c(0, omega-25), zlim = c(0, omega-25), 
-		aspect = c(x, y, z), box = FALSE, axes = FALSE, xlab = "", ylab = "", zlab = "")
-
-# need a way to move parallax w mouse roller
-
-
-
-
-# draw triangle:
-#rgl.triangles(xyztern$x,xyztern$y,xyztern$z,alpha=0.1,color = gray(.8),
-#		ambient="black",specular="black",emission="black",shininess=0)
-
-#draw quad:
-#rgl.quads(xyztern$x,xyztern$y,xyztern$z,alpha=0.3,color = gray(.8),
-#		ambient="black",specular="white",emission="black")
-
-
-
-
---
+++ IndepDyads/R/Tetra_to_indep.R
@@ -0,0 +1,22 @@
+
+# Author: tim
+###############################################################################
+if (system("hostname",intern=TRUE) %in% c("triffe-N80Vm", "tim-ThinkPad-L440")){
+	# if I'm on the laptop
+	setwd("/home/tim/git/IndepDyads/IndepDyads")
+} 
+
+# try to make gif animation of a tetrahedron to independant dyad cross.
+
+AssignColour <- function (x) {
+	if (x == "A") result <- "#D23737"
+	if (x == "P") result <- "#3191C9"
+	if (x == "C") result <- "#D2BC2D"
+	if (x == "T") result <- "#4EC93B"
+	if (x == "D") result <- "#881F93"
+	if (x == "L") result <- "#C5752B"
+	return(result)
+}
+# see /home/tim/workspace/Other/APCTgifs/tetrahedrongif.R
+# for tweenr demonstration
+library(tweenr)

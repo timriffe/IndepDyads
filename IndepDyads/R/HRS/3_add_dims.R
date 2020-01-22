f4bfa9c72ba3f64f7e71a66d6391801ca9c97b88
Tim Riffe <tim.riffe@gmail.com>
2020-01-22 15:16:27 +0100

    testing bimedian slices

--- IndepDyads/R/HRS/3_add_dims.R
+++ IndepDyads/R/HRS/3_add_dims.R
@@ -13,6 +13,8 @@ Dat <- Dat %>%
 		   L = A + TT
 		   )
 
+# orthoganal controls:
+
 
 
 sliceAPCTDL <- function(data, 
@@ -35,6 +37,62 @@ sliceAPCTDL <- function(data,
 		theme(legend.position = "none")
 }
 
+# testing bimedian slices, so far hmm.
+# equivalent:
+# LP bimedian line (D - C) / 2 - P = L / 2 - P
+# AD bimedian line (P - C) / 2 - D = A / 2 - D
+# TC bimedian line (D - P) / 2 - C = T / 2 - C
+Dat <- 
+	Dat %>% 
+	mutate(LPnorm = round((D + C) / 2 - P),
+		   ADnorm = round((P + C) / 2 - D),
+		   TCnorm = round((D + P) / 2 - C))
+range(Dat$LPnorm)
+range(Dat$ADnorm)
+range(Dat$TCnorm)
+# LP
+sliceAPCTDL(Dat,
+			.varname = "adl3",
+			.Sex = "m",
+			abcissae =L,
+			ordinate = P,
+			slider = LPnorm,
+			slider_value = -35)
+
+sliceAPCTDL(Dat,
+			.varname = "adl3",
+			.Sex = "m",
+			abcissae =L,
+			ordinate = P,
+			slider = ADnorm,
+			slider_value = -45)
+# AD
+sliceAPCTDL(Dat,
+			.varname = "adl3",
+			.Sex = "m",
+			abcissae =A,
+			ordinate = D,
+			slider = ADnorm,
+			slider_value = -45)
+
+head(Dat,20)
+# AD
+sliceAPCTDL(Dat,
+			.varname = "adl3",
+			.Sex = "m",
+			abcissae =A,
+			ordinate = D,
+			slider = ADnorm,
+			slider_value = -45)
+# TC
+head(Dat)
+sliceAPCTDL(Dat,
+			.varname = "adl3",
+			.Sex = "m",
+			abcissae = TT,
+			ordinate = C,
+			slider = TCnorm,
+			slider_value = 85)
 
 sliceAPCTDL(Dat,
 			.varname = "adl3",
@@ -44,7 +102,7 @@ sliceAPCTDL(Dat,
 			slider = P,
 			slider_value = p)
 
-
+# end bimedian testing
 
 
 # ------ CT ------- #
